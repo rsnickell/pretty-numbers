6ad73c81060b4652489c5ec5cd86ee1971d4c97d
rsnickell <rsnickell@gmail.com>
2013-04-08 23:52:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+Pretty Number - a jQuery plugin
+===============================
+
+This jQuery plugin provides a way to format a large number in a pretty 
+human readable format.  For example, 100000 (one hundred thousand) would 
+be printed as 100K.
+
+Installation
+------------
+
+In your head tag, include jQuery and this plugin:
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="jquery.prettyNumbers.js" type="text/javascript"></script>
+
+Use
+===
+
+HTML
+----
+
+    <div class="pretty-number">123456</div>
+
+JavaScript
+----------
+
+    $( function() {
+        $('.pretty-number').prettNumber();
+    });
+    
+Resulting HTML
+--------------
+
+    <div class="pretty-number" data-pretty-number="123456" title="123456">1.2K</div>
+
+Supported Element Types
+-----------------------
+
+Elements modifiable by prettyNumber include: div, span, a
+
+If needed, it can easily be modified to add support for additional element types.
+
+The input element is specifically excluded from this list as the intention of the plugin
+is purely for presentation of data, not for data input.
+
---
+++ src/jquery.prettyNumber.js
@@ -0,0 +1,106 @@
+(function( $ ){
+
+    /**
+    ** Defines method lookup for specific element types.
+    **/
+    var elementMethods = {
+        'DIV': function( $element ) {
+            handleTextElement( $element );
+        },
+        'SPAN': function( $element ) {
+            handleTextElement( $element );
+        },
+        'A': function( $element ) {
+            handleTextElement( $element );
+        }
+    };
+
+    /**
+    ** Handles any html element that has a text node.
+    **/
+    function handleTextElement( $element ) {
+        var number = $element.text();
+        setElementAttributes( $element, number, function( prettyNumber ){
+            if( prettyNumber ) {
+                $element.text( prettyNumber );
+            }
+        });
+    }
+
+    /**
+    ** Sets element attributes before invoking the callback to set the pretty number.
+    ** In an effort to not destroy any existing numbers, we create a data-pretty-number
+    ** attribute with the original number and create a title that can display the
+    ** original value when the element is hovered.
+    **/
+    function setElementAttributes( $element, number, callback ) {
+        var prettyNumber;
+        if( number && number.length > 0 && !isNaN( number ) && !$element.data('pretty-number') ) {
+            prettyNumber = prettyPrintNumber( number );
+            $element.attr( 'data-pretty-number', number );
+            $element.attr( 'title', number );
+        }
+        if( callback ) {
+            callback( prettyNumber );
+        }
+    }
+
+    /**
+    ** Method to transform a number into a pretty number string.
+    ** A pretty number example would be 1234 transformed into 1.2K.
+    **
+    ** This currently supports thousands(K), millions(M), billions(B)
+    ** and trillions(T).
+    **
+    ** Maximum decimals is not configurable and at max will be 1 decimal
+    ** place.
+    **/
+    function prettyPrintNumber( number ) {
+        var numberString;
+        var scale = '';
+        if( isNaN( number ) || !isFinite( number ) ) {
+            numberString = 'N/A';
+        } else {
+            var negative = number < 0;
+            number = negative? -number : number;
+
+            if( number < 1000 ) {
+                scale = '';
+            } else if( number < 1000000 ) {
+                scale = 'K';
+                number = number/1000;
+            } else if( number < 1000000000 ) {
+                scale = 'M';
+                number = number/1000000;
+            } else if( number < 1000000000000 ) {
+                scale = 'B';
+                number = number/1000000000;
+            } else if( number < 1000000000000000 ) {
+                scale = 'T';
+                number = number/1000000000000;
+            }
+            var maxDecimals = 0;
+            if( number < 10 && scale != '' ) {
+                maxDecimals = 1;
+            }
+            number = negative ? -number : number;
+            numberString = number.toFixed( maxDecimals );
+            numberString += scale
+        }
+        return numberString;
+    }
+
+    /**
+    ** The pretty number formatting method extending jquery.
+    ** Elements modifiable by prettyNumber include: div, span, a
+    **/
+    $.fn.prettyNumber = function() {
+        return this.each(function() {
+            var $this = $(this);
+            if( elementMethods[ this.tagName ] ) {
+                elementMethods[ this.tagName ]( $this );
+            }
+        });
+    };
+
+})( jQuery );
